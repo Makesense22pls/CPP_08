5f676f8d43040372ae8f21c15d4a8c300eeda99d
Maxence <maxencefournier@MacBook-Air-de-Maxence.local>
2026-02-16 15:52:46 +0100

    push

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -8,14 +8,18 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack() : std::stack<T>() {};
-	MutantStack(const MutantStack& other) : std::stack<T>(other) {};
+	MutantStack() : std::stack<T>() {
+
+	};
+	MutantStack(const MutantStack& other) : std::stack<T>(other) {
+
+	};
 	MutantStack& operator=(const MutantStack& other) {
 		if (this != &other)
 			std::stack<T>::operator=(other);
 		return *this;
 	};
-	~MutantStack() {}
+	~MutantStack() {};
 
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
